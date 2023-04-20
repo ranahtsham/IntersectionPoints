424f66329a460ac76c56681bbc6817d070628e75
Rana Ahtsham Ul Haq <ranahtsham.ulhaq@gmail.com>
2023-04-21 02:26:35 +0500

    complete

--- main.js
+++ main.js
@@ -57,13 +57,14 @@ function getIntersection(A, B, C, D) {
     // Iy = Ay + (By - Ay)t = Cy + (Dy - Cy)u
     */
 
-    const top = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
     const bottom = (D.y - C.y) * (B.x - A.x) - (D.x -C.x) * (B.y - A.y);
-    const t = top / bottom;
 
     if (bottom != 0){
-        const t = top / bottom;
-        if (t >= 0 && t <= 1){
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1){
             return {
                 x: lerp(A.x, B.x, t),
                 y: lerp(A.y, B.y, t),
